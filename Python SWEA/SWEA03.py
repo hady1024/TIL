a667de73e8569a52a91d57731cd9ca10d94eb818
hady1024 <hady1024@gmail.com>
2022-07-20 00:11:54 +0900

    SWEA 문제

--- Python SWEA/SWEA03.py
+++ Python SWEA/SWEA03.py
@@ -5,8 +5,9 @@ sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
-for test_case in range(1, T + 1):
-    numbers = []
+
+for i in range(1,T+1):
+    numbers= []
     numbers = map(int, input().split())
-    average = round(sum(numbers) / 10)
-    print(f'#{test_case} {average}')
+    average = round(sum(numbers) / 10) 
+    print(f'#{i} {average}')
