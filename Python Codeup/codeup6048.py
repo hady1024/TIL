2b8d4693750a8565ea3c248e99fba630d273480d
hady1024 <hady1024@gmail.com>
2022-08-07 20:48:48 +0900

    Code up

---
+++ Python Codeup/codeup6048.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+numbers = a, b
+
+for i in numbers:
+    if a < b:
+        print(True)
+        break
+    
+    else:
+        a >= b
+        print(False)
+        break
