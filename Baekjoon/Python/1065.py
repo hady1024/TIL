b98e093f1bb8dfdab899451457c882a55b957ee5
hady1024 <hady1024@gmail.com>
2022-08-17 21:47:52 +0900

    Baekjoon Update

--- Baekjoon/Python/1065.py
+++ Baekjoon/Python/1065.py
@@ -1,11 +1,10 @@
-N = int(input())
-hansu = 0
-
-for i in range(1, N+1) :
-    if i <= 100 : 
-        hansu += 1 
-    
-    else :     
-        nums = list(map(int, str(i))) 
-        if nums[0] - nums[1] == nums[1] - nums[2] : #등차수열 확인
-            hansu+=1
+n = int(input())
+han = 0
+for i in range(1, n + 1):
+    if i < 100:
+        han += 1
+    else:
+        ns = list(map(int, str(i)))
+        if ns[0] - ns[1] == ns[1] - ns[2]:
+            han += 1
+print(han)
