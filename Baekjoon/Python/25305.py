f73a6983af03ae0479e48d492b3eced77d798a4c
hady1024 <hady1024@gmail.com>
2023-02-14 23:35:50 +0900

    Baekjoon Update

--- Baekjoon/Python/25305.py
+++ Baekjoon/Python/25305.py
@@ -1,6 +1,6 @@
-n,k = map(int,input().split())
-scores = list(map(int,input().split()))
+n, k = map(int, input().split())
+scores = list(map(int, input().split()))
 
 scores.sort(reverse=True)
 
-print(scores[k-1])
+print(scores[k - 1])
