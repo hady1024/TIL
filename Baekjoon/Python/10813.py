ead2f9a1647dd548b609873a11eb7a724a5d59ea
hady1024 <hady1024@gmail.com>
2023-08-31 23:16:06 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/10813.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+box = [i for i in range(1, n + 1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    temp = box[i - 1]
+    box[i - 1] = box[j - 1]
+    box[j - 1] = temp
+
+for b in box:
+    print(b, end=" ")
+# 다시 풀어보기;;
