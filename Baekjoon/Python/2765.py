c7a410a9fb6485d9a22414e1f59efad1cc0bf9cb
hady1024 <hady1024@gmail.com>
2023-09-06 20:39:42 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/2765.py
@@ -0,0 +1,15 @@
+from math import pi
+import sys
+
+input = sys.stdin.readline
+
+# 입력
+i = 1
+while True:
+    d, r, t = map(float, input().split())
+    if r == 0:
+        break
+    dis = d / 63360 * pi * r
+    mph = dis / t * 3600
+    print("Trip #%d: %.2f %.2f" % (i, dis, mph))
+    i += 1
