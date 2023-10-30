e357f8fc8c1c9248cce35556bf9ff80bf4012f09
hady1024 <hady1024@gmail.com>
2023-10-30 22:19:26 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/24416.py
@@ -0,0 +1,15 @@
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def fibonacci(n):
+    return n - 2
+
+
+n = int(input())
+print(fib(n), fibonacci(n))
+
+# 다시 풀어보기
