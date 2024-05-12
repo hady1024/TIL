5e1eecd1dc15186187968f11e3386115c2665798
hady1024 <hady1024@gmail.com>
2024-05-12 23:46:16 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/2153.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n == 1:
+        return "It is a prime word."
+    for i in range(2, n):
+        if n % i == 0:
+            return "It is not a prime word."
+    return "It is a prime word."
+
+s = 0
+for i in input():
+    if i.islower():
+        s += ord(i) - 96
+    else:
+        s += ord(i) - 38
+print(is_prime(s))
+
+# 잘 안됨
