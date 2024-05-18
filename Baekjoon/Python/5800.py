b3ec5577d9f70774851fd6007a55b7311f263d91
hady1024 <hady1024@gmail.com>
2024-05-18 23:22:44 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/5800.py
@@ -0,0 +1,14 @@
+k = int(input())
+student = []
+for j in range(1,k+1):
+    student = list(map(int,input().split()))
+    student = student[1:]
+    s_student = sorted(student)
+    lg = 0
+    for i in range(0,len(s_student)-1): 
+        if s_student[i+1] - s_student[i]>lg:
+            lg = s_student[i+1] - s_student[i]
+    print(f'Class {j}')
+    print(f'Max {max(s_student)}, Min {min(s_student)}, Largest gap {lg}')
+
+# 다시 해봐야함 안됐음;
