989eb900c5a9834e557cdaafaddceeee013abced
hady1024 <hady1024@gmail.com>
2024-10-31 21:56:36 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/3035.py
@@ -0,0 +1,15 @@
+R, C, ZR, ZC = map(int, input().split())
+paper = [input() for _ in range(R)]
+scanner = []
+
+for i in range(R):
+    row = []
+    for j in range(C):
+        row.append(paper[i][j] * ZC)
+    for _ in range(ZR):
+        scanner.append(row)
+
+for s in scanner:
+    print("".join(s))
+
+# 참고한것 다시 해보기
