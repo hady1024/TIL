3079a87902dd57399ce3fc6cbaa36970a01a7ead
hady1024 <hady1024@gmail.com>
2024-11-13 22:23:13 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/27889.py
@@ -0,0 +1,6 @@
+dic = {'NLCS': 'North London Collegiate School', 'BHA': 'Branksome Hall Asia', 
+       'KIS': 'Korea International School', 'SJA': 'St. Johnsbury Academy'}
+short = input()
+print(dic[short])
+
+# 참고한것 
