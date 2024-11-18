9479e5d05ef9b75d90ab246aa7a0a3e9fbc30ddb
hady1024 <hady1024@gmail.com>
2024-11-18 19:05:39 +0900

    Baekjoon Update

--- Baekjoon/Python/27889.py
+++ Baekjoon/Python/27889.py
@@ -3,3 +3,21 @@ dic = {'NLCS': 'North London Collegiate School', 'BHA': 'Branksome Hall Asia',
 short = input()
 print(dic[short])
 
+
+
+# 2 다른사람 풀이 (참고한것)
+def special_school_name(name):
+    school_dict = {
+        "NLCS": "North London Collegiate School",
+        "BHA": "Branksome Hall Asia",
+        "KIS": "Korea International School",
+        "SJA": "St. Johnsbury Academy"
+    }
+    
+    return school_dict[name]
+
+
+if __name__ == "__main__":
+    school_name = input()
+    
+    print(special_school_name(name=school_name))
