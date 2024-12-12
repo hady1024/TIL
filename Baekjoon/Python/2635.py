53a91575e1399cbebffcd5dd063a2482ab0ea4ff
hady1024 <hady1024@gmail.com>
2024-12-12 21:21:37 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/2635.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+result = [N]
+for i in range(1, N+1) :
+  tmp = [N, i]
+  while tmp[-1] >= 0 :
+    tmp.append(tmp[-2] - tmp[-1])
+  tmp.pop()
+  if len(tmp) > len(result) :
+    result = tmp
+
+print(len(result))
+print(*result)
+
+# 참고한것 다시 풀어보기
