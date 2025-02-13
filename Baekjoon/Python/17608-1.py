fe39d1ca095fc67ffe2a625751d26aab33e768b5
hady1024 <hady1024@gmail.com>
2025-02-13 21:26:44 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/17608-1.py
@@ -0,0 +1,15 @@
+N = int(input())
+stack = []
+
+for _ in range(N):
+    stack.append(int(input()))
+
+last = stack[-1]
+count = 1
+
+for i in reversed(range(N)):
+    if stack[i] > last:
+        count += 1
+        last = stack[i]
+
+print(count)
