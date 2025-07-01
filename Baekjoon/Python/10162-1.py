d53dbf93e54a1e4aeb3e4f0c70b51ef68719e7b6
hady1024 <hady1024@gmail.com>
2025-07-01 23:28:56 +0900

    Baekjoon Update

--- Baekjoon/Python/10162-1.py
+++ Baekjoon/Python/10162-1.py
@@ -7,19 +7,3 @@ else:
 		print(n//i, end=' ')
 		n = n%i
 
-# 2
-
-btn = [300, 60, 10]
-cnt = []
-t = int(input())
-
-for i in range(3):
-    bCnt = t // btn[i]
-    t %= btn[i]
-    cnt.append(bCnt)
-
-if t == 0:
-    for c in cnt:
-        print(c, end=' ')
-else:
-    print(-1)
