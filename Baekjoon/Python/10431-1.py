9126ef13fb07725c3049afbf25decbc5e0503fbb
hady1024 <hady1024@gmail.com>
2025-07-10 20:55:10 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/10431-1.py
@@ -0,0 +1,24 @@
+p =int(input())
+
+for k in range(1,p+1):
+    height_sort = []
+    height = list(map(int, input().split()))
+    ans = 0
+    for i in range(1,len(height)):
+        if i == 1:
+            height_sort.append(height[i])
+            continue
+
+        now = height[i]
+        if now > max(height_sort):
+            height_sort.append(now)
+            continue
+
+        for j in range(len(height_sort)):
+            if height_sort[j] > now:
+                height_sort.insert(j, now)
+                ans += (len(height_sort) - j - 1)
+                break
+    print(k,ans)
+    
+# 다시 해보기
