b21501bb120cdf8cf8cac2fdd0e78dd4e3000225
hady1024 <hady1024@gmail.com>
2025-07-21 21:47:32 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/1059.py
@@ -0,0 +1,21 @@
+L = int(input())
+S = list(map(int,input().split()))
+n = int(input())
+
+S.sort()
+
+if n in S:
+    print(0)
+else:
+    min = 0
+    max = 0
+    for num in S:           
+        if num < n:     
+            min = num
+        elif num > n and max == 0:
+            max = num
+    max -= 1                 
+    min += 1
+    print((n-min)*(max-n+1) + (max-n))
+
+# 다시 해보기
