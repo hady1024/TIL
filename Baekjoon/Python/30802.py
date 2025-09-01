37201121da79b00c665c11b1a781909a42b550b8
hady1024 <hady1024@gmail.com>
2025-09-01 20:30:48 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/30802.py
@@ -0,0 +1,19 @@
+N = int(input())
+size = list(map(int, input().split()))
+T, P = map(int, input().split())
+
+shirt_bundle = 0
+for sz in size:
+  if sz == 0:
+    continue
+  elif sz <= T:
+    shirt_bundle += 1
+  elif sz % T == 0:
+    shirt_bundle += (sz // T)
+  else:
+    shirt_bundle += (sz // T) + 1
+
+print(shirt_bundle)
+print(N//P, N%P)
+
+# 다시 해보기 
