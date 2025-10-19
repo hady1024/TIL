7713bf9afa922488bfe30fef70bf322f50645e87
hady1024 <hady1024@gmail.com>
2025-10-19 17:08:09 +0900

    Baekjoon Update

---
+++ C++/1008.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+int main(int argc, char const *argv[]) {
+ 
+    double a, b;
+    cin >> a >> b;
+ 
+    cout.precision(12);
+    cout << fixed;
+    cout << a / b;
+    return 0;
+}
+
+// 다시 해보기
