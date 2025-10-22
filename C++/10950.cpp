03547da4aa01d13adf994f4c9ba9b7fb6d3159c1
hady1024 <hady1024@gmail.com>
2025-10-22 21:31:23 +0900

    Baekjoon Update

---
+++ C++/10950.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+	int T, A, B;
+ 
+	cin >> T; 
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+	return 0;
+}
+// 다시 해보기
