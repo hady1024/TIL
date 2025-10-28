8967db2e8777b23478913236c68ca255c4ea78ec
hady1024 <hady1024@gmail.com>
2025-10-28 20:36:07 +0900

    Baekjoon Update

---
+++ C++/2739.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+	int N;
+	cin >> N;
+ 
+	for (int i = 1; i < 10; i++) {
+		cout << N << " * " << i << " = " << N * i << "\n";
+	}
+	return 0;
+}
+
+// 다시 해보기 
