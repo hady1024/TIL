824380ec63b8374d4595696edbc7d897493d2d8c
hady1024 <hady1024@gmail.com>
2025-11-10 18:36:00 +0900

    Baekjoon Update

---
+++ C++/10430.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(int argc, char const *argv[]) {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+ 
+    printf("%d\n", (A + B) % C);
+    printf("%d\n", (A % C + B % C) % C);
+    printf("%d\n", (A * B) % C);
+    printf("%d\n", (A % C * B % C) % C);
+    return 0;
+}
+
+// 다시 해보기 
