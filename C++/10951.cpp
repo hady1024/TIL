62675c6455e3d9321831af69a2f463f99567d94c
hady1024 <hady1024@gmail.com>
2025-12-03 22:12:41 +0900

    Baekjoon Update

---
+++ C++/10951.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+ 
+int main(int argc, const char * argv[]) {
+ 
+	int a, b;
+ 
+
+	while(scanf("%d %d", &a, &b) != -1) {
+		printf("%d\n", a + b);
+	}
+ 
+	return 0;
+}
+
+// 참고한것 while 문 다시 해보기
