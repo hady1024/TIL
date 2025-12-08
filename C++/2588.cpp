ef0ea1d52171f9ec185e5f9bd250f895ec8f7171
hady1024 <hady1024@gmail.com>
2025-12-08 22:45:05 +0900

    Baekjoon Update

---
+++ C++/2588.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+    int A, B;
+ 
+    cin >> A >> B;
+ 
+    cout << A * (B % 10) << "\n";
+    cout << A * ((B % 100) / 10) << "\n";
+    cout << A * (B / 100) << "\n";
+    cout << A * B;
+ 
+    return 0;
+}
+
+// 참고한것 다시 해보기
