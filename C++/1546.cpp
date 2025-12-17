c1c9b9e1a25951bfcb596818ac975701ddfad4fa
hady1024 <hady1024@gmail.com>
2025-12-17 23:29:55 +0900

    Baekjoon Update

---
+++ C++/1546.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	double jumsu[1001];
+	int t, max = -1;
+	double sum = 0;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> jumsu[i];
+		if (max < jumsu[i]) max = jumsu[i];
+	}
+
+	for (int i = 0; i < t; i++) {
+		jumsu[i] = jumsu[i] / max * 100;
+		sum += jumsu[i];
+	}
+
+	cout << sum / t;
+	
+	
+
+	return 0;
+}
+
+// 다시 풀어보기
