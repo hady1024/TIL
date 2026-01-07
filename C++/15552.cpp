659331f128950aab001d4b090c5d972e64c06f81
hady1024 <hady1024@gmail.com>
2026-01-07 23:08:40 +0900

    Baekjoon Update

---
+++ C++/15552.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(int argc, char const *argv[]) {
+ 
+	int T, a, b;
+ 
+	scanf("%d", &T);
+	for(int i = 0; i < T; i++){
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+ 
+	return 0;
+}
+
+// 다시 해보기 잘 안됐음 
