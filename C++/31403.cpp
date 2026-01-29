34849115fa3431898e352b9c58bcd963148c8586
hady1024 <hady1024@gmail.com>
2026-01-29 16:45:29 +0900

    Baekjoon Update

---
+++ C++/31403.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string A, B, C;
+
+    cin >> A >> B >> C;
+
+    cout << stoi(A) + stoi(B) - stoi(C) << '\n';
+    cout << stoi(A + B) - stoi(C) << '\n';
+
+    return 0;
+}
+
+// 다시 해보기
