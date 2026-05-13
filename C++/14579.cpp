9475bb97202c5e996b975984b6e9d845f283db1c
hady1024 <hady1024@gmail.com>
2026-05-13 21:51:59 +0900

    Baekjoon Update

---
+++ C++/14579.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+
+    int sum = 0;
+
+    for(int i=0; i<=A; i++){
+        sum += i;
+    }
+
+    int tmp = sum;
+
+    for(int i=A+1; i<=B; i++){
+        tmp += i;
+        sum *= tmp;
+        sum %= 14579;
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
+
+// 다시 해보기
