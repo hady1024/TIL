f7be5b0ecf3485f7674debca74a42e8754f7fef0
hady1024 <hady1024@gmail.com>
2026-05-21 19:46:02 +0900

    Baekjoon Update

---
+++ Baekjoon/Python/14491-1.py
@@ -0,0 +1,13 @@
+def solve(r, n):
+    if n == 0:
+        return r
+    
+    if n%9:
+        return solve(str(n%9)+r, n//9)
+    else:
+        return solve("0"+r, n//9)
+print(solve("", int(input())))
+
+
+# 다시 해보기
+        
