733939fb0a0bd865c4fcc86ebedae3bd2b5d30c1
hady1024 <hady1024@gmail.com>
2026-07-01 20:14:50 +0900

    Baekjoon Update

---
+++ C++/5086.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int a, b;
+  cin >> a >> b;
+
+  while(a != 0 && b != 0) {
+    if(b % a == 0) 
+      cout << "factor" << '\n';
+    else if(a % b == 0)
+      cout << "multiple" << '\n';
+    else
+      cout << "neither" << '\n';
+
+    cin >> a >> b;
+  }
+
+  return 0;
+}
+
+// 참고한것 다시해보기
