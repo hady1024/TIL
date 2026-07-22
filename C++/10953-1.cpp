632a703094a7f22d8a58311deb538d2c608c8f53
hady1024 <hady1024@gmail.com>
2026-07-22 20:53:39 +0900

    Baekjoon Update

---
+++ C++/10953-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main() {
+    cin >> T;
+
+    while(T--) {
+        string s;
+        cin >> s;
+
+        int add = s[0] + s[2] - 96; // 96 = '0'
+        cout << add << "\n";
+    }
+
+}
+
+// 다시 풀어보기 
