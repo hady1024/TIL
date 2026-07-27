31f05c7f0d17cf04a73d14f7ed2b271bc6fae2c7
hady1024 <hady1024@gmail.com>
2026-07-27 22:18:15 +0900

    Baekjoon Update

---
+++ C++/16561.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n; cin >> n;
+
+    n = n / 3 - 2;
+
+    int res = 0;
+
+    for (int i = 1; i <= n; i++) {
+        res += i;
+    }
+
+    cout << res;
+
+    return 0;
+}
+
+// 다시 해보기
