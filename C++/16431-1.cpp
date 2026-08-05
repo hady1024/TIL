4c783d1044737c25ceb71e7d7997b4c2eb69c4e9
hady1024 <hady1024@gmail.com>
2026-08-05 18:35:21 +0900

    Baekjoon Update

---
+++ C++/16431-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int br,bc,dr,dc,lr,lc;
+    cin>>br>>bc>>dr>>dc>>lr>>lc;
+    int m=min(abs(lr-br),abs(lc-bc));
+    int sum=abs(lr-br)+abs(lc-bc)-m;
+    int sum1=abs(lr-dr)+abs(lc-dc);
+
+    if(sum>sum1) cout<<"daisy";
+
+    else if(sum<sum1) cout<<"bessie";
+    else cout<<"tie";
+}
+
+// 참고한것 다시해보기
