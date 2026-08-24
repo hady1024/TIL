ef4dbdbafee7d161e721a5d0d051d1d42648be55
hady1024 <hady1024@gmail.com>
2026-08-24 22:35:32 +0900

    Baekjoon Update

---
+++ C++/15873.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0, sum = 0;
+    cin >> N;
+    
+    if((N % 10) != 0){
+        sum += (N % 10);
+        N /= 10;
+
+    }
+    else{
+        sum += 10;
+        N /= 100;
+    }
+
+    sum += N;
+    
+    cout << sum << '\n';
+    
+    return 0;
+}
+
+// 참고한것 다시 해보기 
