326349d4bcb8d6fae9e0d3c09753a8d1e4565504
hady1024 <hady1024@gmail.com>
2026-09-02 21:28:12 +0900

    Baekjoon Update

---
+++ C++/11023.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    int ans = 0;
+    while (cin >> n) {
+        ans += n;
+    }
+    cout << ans;
+}
+
+
+// 참고한것 다시 해보기
