ebd5bf27c63c6435f947fe89ccb89ca3f88fa27a
hady1024 <hady1024@gmail.com>
2026-09-07 20:43:18 +0900

    Baekjoon Update

---
+++ C++/11023-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n,ans = 0;
+    while(~scanf("%d", &n))
+    {
+        ans += n;
+    }
+    cout << ans << '\n';
+}
+
+// 다시 해보기
